8653520e79ba9829c68980f967a7b1dce936c74c
DESKTOP-9381VTO\Imagemaker <davsonsarabia20@gmail.com>
2021-09-19 16:03:39 -0300

    Revert "Comentario de prueba 1 sourstre y gitlab"
    This reverts commit 0feb4d73022c68b20267420c67de03c4ebd616a9.

--- src/test/java/PruebaNivelMedio/test/SitioSpotify.java
+++ src/test/java/PruebaNivelMedio/test/SitioSpotify.java
@@ -5,8 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SitioSpotify {
-    //prueba sourctre y gitlab//
-
+    
 
     public WebDriver driver;
 
