0feb4d73022c68b20267420c67de03c4ebd616a9
DESKTOP-9381VTO\Imagemaker <davsonsarabia20@gmail.com>
2021-09-19 15:42:01 -0300

    Comentario de prueba 1 sourstre y gitlab

--- src/test/java/PruebaNivelMedio/test/SitioSpotify.java
+++ src/test/java/PruebaNivelMedio/test/SitioSpotify.java
@@ -5,7 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SitioSpotify {
-    
+    //prueba sourctre y gitlab//
+
 
     public WebDriver driver;
 
